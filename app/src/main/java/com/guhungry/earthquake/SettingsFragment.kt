e80caff23b3975ddf1de0b2e39ff05ecd59fa434
Woraphot Chokratanasombat <wc@asiola.com>
2018-06-27 12:15:02 +0700

    [Quake Setting] Change: Show summary

--- app/src/main/java/com/guhungry/earthquake/SettingsFragment.kt
+++ app/src/main/java/com/guhungry/earthquake/SettingsFragment.kt
@@ -1,13 +1,37 @@
 package com.guhungry.earthquake
 
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
+import android.content.SharedPreferences
 
-class SettingsFragment : PreferenceFragment() {
+
+
+class SettingsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         addPreferencesFromResource(R.xml.settings_main)
+        bindPreferenceSummaryToValue(findPreference(R.string.settings_min_magnitude_key))
+    }
+
+    fun findPreference(key: Int): Preference = findPreference(getString(key))
+
+    private fun bindPreferenceSummaryToValue(preference: Preference) {
+        preference.onPreferenceChangeListener = this
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(preference.context)
+        val preferenceString = preferences.getString(preference.key, "")
+        onPreferenceChange(preference, preferenceString)
+    }
+
+    // //////////////////////////
+    // OnPreferenceChangeListener
+    // //////////////////////////
+    override fun onPreferenceChange(preference: Preference?, value: Any?): Boolean {
+        preference?.summary = value.toString()
+        return true
     }
 
     companion object {
